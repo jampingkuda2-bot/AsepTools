6de1b04e3cf3f7d1a200ded9adc6932e15157260
AlexAlstr <jampingkuda2@gmail.com>
2026-07-31 15:12:39 +0700

    Delete api directory

--- api/auth/me.js
+++
@@ -1,9 +0,0 @@
-const { getSessionFromReq } = require("../../lib/auth");
-
-module.exports = async (req, res) => {
-  const session = getSessionFromReq(req);
-  if (!session) return res.status(200).json({ user: null });
-  res.status(200).json({
-    user: { id: session.uid, email: session.email, name: session.name, avatar: session.avatar },
-  });
-};
